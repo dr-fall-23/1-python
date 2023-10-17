d55a16354aa8125f9bc3cb1200314cd0ce6b5c78
digitalready-george <140411573+digitalready-george@users.noreply.github.com>
2023-10-17 15:15:37 -0400

    restructure

---
+++ daily-wrapup/dw_10_13_23.py
@@ -0,0 +1,16 @@
+from typing import List
+
+# Write the funciont "common_end"
+# Given two lists a and b, return True
+# if they have the same first element OR
+# if they have the same last element
+# For example:
+# common_ends([1, 2, 3], [5, 3]) -> True
+# common_ends([1, 2, 3], [1, 4, 56, 7, "Hello"]) -> True
+# common_ends([1, 2, 3], [False]) -> False
+
+def common_ends(a: List, b: List) -> bool:
+    # Write your code here!
+
+# Test your code here!
+
